6fcd04bddc8397ffe1b7af968c6a9fddf8e0d439
Topan Oana <91469778+TopanOana@users.noreply.github.com>
2023-03-27 21:02:27 +0300

    some stuff added

---
+++ Semester4/Database Management Systems/LABS/DBMSLab1/Lab1/DBMSLab1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace DBMSLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.updateButton = new System.Windows.Forms.Button();
+            this.dataGridViewBrands = new System.Windows.Forms.DataGridView();
+            this.dataGridViewProducts = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBrands)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(432, 588);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(91, 61);
+            this.updateButton.TabIndex = 0;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // dataGridViewBrands
+            // 
+            this.dataGridViewBrands.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBrands.Location = new System.Drawing.Point(31, 87);
+            this.dataGridViewBrands.Name = "dataGridViewBrands";
+            this.dataGridViewBrands.RowHeadersWidth = 51;
+            this.dataGridViewBrands.RowTemplate.Height = 24;
+            this.dataGridViewBrands.Size = new System.Drawing.Size(394, 429);
+            this.dataGridViewBrands.TabIndex = 1;
+            // 
+            // dataGridViewProducts
+            // 
+            this.dataGridViewProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProducts.Location = new System.Drawing.Point(517, 87);
+            this.dataGridViewProducts.Name = "dataGridViewProducts";
+            this.dataGridViewProducts.RowHeadersWidth = 51;
+            this.dataGridViewProducts.RowTemplate.Height = 24;
+            this.dataGridViewProducts.Size = new System.Drawing.Size(394, 429);
+            this.dataGridViewProducts.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Brands";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(514, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Products";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1154, 715);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewProducts);
+            this.Controls.Add(this.dataGridViewBrands);
+            this.Controls.Add(this.updateButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBrands)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProducts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.DataGridView dataGridViewBrands;
+        private System.Windows.Forms.DataGridView dataGridViewProducts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Semester4/Database Management Systems/LABS/DBMSLab1/Lab1/DBMSLab1/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBMSLab1
+{
+    public partial class Form1 : Form
+    {
+        //connection to database
+        SqlConnection sqlConnection;
+        //data adapters for tables
+        //the bridge between the dataset and the sql server
+        //used to obtain data and save the changes back in the database
+        SqlDataAdapter brandsDataAdapter;
+        SqlDataAdapter productsDataAdapter;
+        //object that contains the tables 
+        //temporary storage for use in application
+        //local in-memory cache
+        DataSet dataSet;
+
+        //binds the data to the application
+        //what you see in the application
+        BindingSource brandsBindingSource;
+        BindingSource productsBindingSource;
+
+        SqlCommandBuilder sqlCommandBuilder;
+
+        string brandsQuery;
+        string productsQuery;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fillData();
+        }
+
+        void fillData()
+        {
+            //SQLConnection initialization
+            sqlConnection = new SqlConnection(getConnectionString());
+
+            //queries for the tables
+            productsQuery = "SELECT * FROM PRODUCTS";
+            brandsQuery = "SELECT * FROM BRANDS";
+
+            //DataAdapters
+            productsDataAdapter = new SqlDataAdapter(productsQuery,sqlConnection);
+            brandsDataAdapter = new SqlDataAdapter(brandsQuery,sqlConnection);
+
+            //DataSet
+            dataSet = new DataSet();
+
+            //Fill the data adapters with the data from the dataset
+            productsDataAdapter.Fill(dataSet, "Products");
+            brandsDataAdapter.Fill(dataSet, "Brands");
+
+
+            sqlCommandBuilder = new SqlCommandBuilder(productsDataAdapter);
+
+            //add the parent-child relation between brands and products
+            //add it on the dataset
+            dataSet.Relations.Add("BrandsProduct",
+                dataSet.Tables["Brands"].Columns["id"],
+                dataSet.Tables["Products"].Columns["brand_id"]);
+
+
+            //binding sources to the data set
+            brandsBindingSource = new BindingSource();
+            brandsBindingSource.DataSource = dataSet.Tables["Brands"];
+            productsBindingSource = new BindingSource(brandsBindingSource, "BrandsProduct");
+
+
+            ///dataGridViews
+            //populate the grid views that you can see in the application
+            //using the binding sources
+            dataGridViewBrands.DataSource = brandsBindingSource; 
+            dataGridViewProducts.DataSource = productsBindingSource;
+
+            ///sqlCommandBuilder
+            sqlCommandBuilder.GetUpdateCommand();
+        }
+
+
+        string getConnectionString()
+        {
+            return "Data Source=DESKTOP-S2N055K; Initial Catalog = SkateShop;"+"Integrated Security=true;";
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            //the dataAdapter checks whether there have been changes made on the database
+            //if so, the respective insert,update or delete commands are executed on the database
+            try{
+                productsDataAdapter.Update(dataSet, "Products");
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
